d6996b9c759c399807f25a54768176738981b268
lenna <lennard.persoon@gmail.com>
2024-01-21 15:18:48 +0100

    -Hoevde niet weg

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -55,7 +55,6 @@ android {
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     defaultConfig {
         applicationId = "com.filecard.multiplatform"
