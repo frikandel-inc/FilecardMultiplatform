bcedb5c84fe49a6ff02eebc4148a325d120c9068
lenna <lennard.persoon@gmail.com>
2024-01-23 20:23:54 +0100

    -Brain ui change 5.0

--- composeApp/src/commonMain/kotlin/ui/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/HomeScreen.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.jetbrains.skiko.URIManager
 
 @Composable
 fun HomeScreen(){
@@ -29,6 +31,19 @@ fun HomeScreen(){
             text = "Wilt u uw downloads zien druk dan op Downloads aan de onderkant van uw scherm",
             style = MaterialTheme.typography.titleMedium,
         )
+        TextButton(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+
+            onClick = { URIManager().openUri("https://www.youtube.com/watch?v=jVv2GznAT_w&ab_channel=BrianSuyker") },
+
+        ) {
+            Text(
+                text = "https://www.youtube.com/watch?v=jVv2GznAT_w&ab_channel=BrianSuyker",
+                style = MaterialTheme.typography.labelLarge
+            )
+        }
+
+
 
 
     }
